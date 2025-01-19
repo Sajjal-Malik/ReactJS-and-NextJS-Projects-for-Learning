6518abb25d3c7cd68b22f6e5521dbeffc6869998
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-01-19 14:13:33 +0500

    Modified 4th Project

--- 4.Password Manager in React/passop/src/App.jsx
+++ 4.Password Manager in React/passop/src/App.jsx
@@ -2,17 +2,16 @@ import Navbar from './components/Navbar'
 import Manager from './components/Manager'
 import Footer from './components/Footer'
 
-function App() {
-
+function App() { 
 
   return (
     <>
-      <Navbar />
-      <div className='min-h-[82vh]'>
-        <Manager />
-      </div>
-      <Footer />
-    
+        <Navbar /> 
+        <div className="bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+
+       <Manager/> 
+        </div>
+       <Footer/>
     </>
   )
 }
--- 4.Password Manager in React/passop/src/components/Footer.jsx
+++ 4.Password Manager in React/passop/src/components/Footer.jsx
@@ -1,12 +1,17 @@
+
 import React from 'react'
 
 const Footer = () => {
     return (
-        <div className=' bg-slate-800 flex text-white justify-center items-center h-12 w-full'>
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-white text-2xl">
+                <span className='text-green-500'> &lt;</span>
 
-            <div className='footer font-bold text-xl'>
-                Created with ❤ Sajjal_Malik
+                <span>Pass</span><span className='text-green-500'>OP/&gt;</span>
+
+
             </div>
+            <div className='flex justify-center items-center'> Created with <img className='w-7 mx-2' src="icons/heart.png" alt="" /> by CodeWithHarry </div>
         </div>
     )
 }
